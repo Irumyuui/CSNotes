c84561140369e4fac6b440c072100d212c3a8140
Irumyuui <ciel_katana@outlook.com>
2024-07-13 23:01:38 +0800

    关闭自动刷新缓冲区

--- Algorithm/TemplateCode/CSharpTemplate.cs
+++ Algorithm/TemplateCode/CSharpTemplate.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 #if DEBUG
 using Dumpify;
@@ -8,7 +9,9 @@ using static System.Math;
 
 void Main(Scanner cin, TextWriter cout)
 {
+
 }
+
 #if DEBUG
 
 /*
@@ -73,7 +76,12 @@ foreach (var (id, question) in questions.WithIndex())
 }
 
 #else
+Console.SetOut(new StreamWriter(Console.OpenStandardOutput())
+{
+    AutoFlush = false,
+});
 Main(new Scanner(Console.In), Console.Out);
+Console.Out.Flush();
 #endif
 
 #region TemplateCode
@@ -85,6 +93,7 @@ public class Scanner
 
     public Scanner(TextReader reader) => _reader = reader;
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     private void ReadNextLine()
     {
         while (_buffer.Count is 0)
@@ -94,6 +103,7 @@ public class Scanner
         }
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public string NextWord()
     {
         ReadNextLine();
@@ -102,59 +112,69 @@ public class Scanner
         return text;
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public T Next<T>(IFormatProvider? p = null) where T : IParsable<T>
     {
         return T.Parse(NextWord(), p);
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2) Next<T1, T2>(IFormatProvider? p1 = null, IFormatProvider? p2 = null) where T1 : IParsable<T1> where T2 : IParsable<T2>
     {
         var t1 = Next<T1>(p1);
         return (t1, Next<T2>(p2));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3) Next<T1, T2, T3>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3>
     {
         var (t1, t2) = Next<T1, T2>(p1, p2);
         return (t1, t2, Next<T3>(p3));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3, T4) Next<T1, T2, T3, T4>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null, IFormatProvider? p4 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3> where T4 : IParsable<T4>
     {
         var (t1, t2, t3) = Next<T1, T2, T3>(p1, p2, p3);
         return (t1, t2, t3, Next<T4>(p4));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3, T4, T5) Next<T1, T2, T3, T4, T5>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null, IFormatProvider? p4 = null, IFormatProvider? p5 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3> where T4 : IParsable<T4> where T5 : IParsable<T5>
     {
         var (t1, t2, t3, t4) = Next<T1, T2, T3, T4>(p1, p2, p3, p4);
         return (t1, t2, t3, t4, Next<T5>(p5));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3, T4, T5, T6) Next<T1, T2, T3, T4, T5, T6>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null, IFormatProvider? p4 = null, IFormatProvider? p5 = null, IFormatProvider? p6 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3> where T4 : IParsable<T4> where T5 : IParsable<T5> where T6 : IParsable<T6>
     {
         var (t1, t2, t3, t4, t5) = Next<T1, T2, T3, T4, T5>(p1, p2, p3, p4, p5);
         return (t1, t2, t3, t4, t5, Next<T6>(p6));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3, T4, T5, T6, T7) Next<T1, T2, T3, T4, T5, T6, T7>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null, IFormatProvider? p4 = null, IFormatProvider? p5 = null, IFormatProvider? p6 = null, IFormatProvider? p7 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3> where T4 : IParsable<T4> where T5 : IParsable<T5> where T6 : IParsable<T6> where T7 : IParsable<T7>
     {
         var (t1, t2, t3, t4, t5, t6) = Next<T1, T2, T3, T4, T5, T6>(p1, p2, p3, p4, p5, p6);
         return (t1, t2, t3, t4, t5, t6, Next<T7>(p7));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3, T4, T5, T6, T7, T8) Next<T1, T2, T3, T4, T5, T6, T7, T8>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null, IFormatProvider? p4 = null, IFormatProvider? p5 = null, IFormatProvider? p6 = null, IFormatProvider? p7 = null, IFormatProvider? p8 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3> where T4 : IParsable<T4> where T5 : IParsable<T5> where T6 : IParsable<T6> where T7 : IParsable<T7> where T8 : IParsable<T8>
     {
         var (t1, t2, t3, t4, t5, t6, t7) = Next<T1, T2, T3, T4, T5, T6, T7>(p1, p2, p3, p4, p5, p6, p7);
         return (t1, t2, t3, t4, t5, t6, t7, Next<T8>(p8));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public (T1, T2, T3, T4, T5, T6, T7, T8, T9) Next<T1, T2, T3, T4, T5, T6, T7, T8, T9>(IFormatProvider? p1 = null, IFormatProvider? p2 = null, IFormatProvider? p3 = null, IFormatProvider? p4 = null, IFormatProvider? p5 = null, IFormatProvider? p6 = null, IFormatProvider? p7 = null, IFormatProvider? p8 = null, IFormatProvider? p9 = null) where T1 : IParsable<T1> where T2 : IParsable<T2> where T3 : IParsable<T3> where T4 : IParsable<T4> where T5 : IParsable<T5> where T6 : IParsable<T6> where T7 : IParsable<T7> where T8 : IParsable<T8> where T9 : IParsable<T9>
     {
         var (t1, t2, t3, t4, t5, t6, t7, t8) = Next<T1, T2, T3, T4, T5, T6, T7, T8>(p1, p2, p3, p4, p5, p6, p7, p8);
         return (t1, t2, t3, t4, t5, t6, t7, t8, Next<T9>(p9));
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     public T[] Next<T>(int length, IFormatProvider? p = null) where T : IParsable<T>
     {
         var arr = new T[length];
@@ -192,7 +212,7 @@ public static class ArrayExtensions
 public static class DebugExtensions
 {
     [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-    public static void Dump<T>(this T self) {}
+    public static void Dump<T>(this T self) { }
 }
 #endif
 
