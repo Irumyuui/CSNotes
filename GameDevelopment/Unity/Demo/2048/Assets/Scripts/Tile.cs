f77c602e3c5b1d0331a8844cc6c21018c5ed8edc
Irumyuui <ciel_katana@outlook.com>
2024-08-11 21:17:06 +0800

    添加 2048 DEMO
    是一个未做完的版本，后面的加一加计分的就懒得弄了。

---
+++ GameDevelopment/Unity/Demo/2048/Assets/Scripts/Tile.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    public TileState state { get; private set; }
+    public TileCell cell { get; private set; }
+    public int number { get; private set; }
+    public bool locked { get; set; }
+
+    private Image background;
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        background = GetComponentInChildren<Image>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetState(TileState state, int number)
+    {
+        this.state = state;
+        this.number = number;
+
+        background.color = state.backgroundColor;
+        text.color = state.textColor;
+        text.text = $"{number}";
+    }
+
+    public void Spawn(TileCell cell)
+    {
+        if (this.cell != null)
+        {
+            this.cell.tile = null;
+        }
+
+        this.cell = cell;
+        this.cell.tile = this;
+
+        transform.position = cell.transform.position;
+    }
+
+    public void MoveTo(TileCell cell)
+    {
+        if (this.cell != null)
+        {
+            this.cell.tile = null;
+        }
+
+        this.cell = cell;
+        this.cell.tile = this;
+
+        // transform.position = cell.transform.position;
+        StartCoroutine(Animate(cell.transform.position));
+    }
+
+    public void Merge(TileCell cell)
+    {
+        if (this.cell != null)
+        {
+            this.cell.tile = null;
+        }
+
+        this.cell = null;
+        cell.tile.locked = true;
+
+        StartCoroutine(Animate(cell.transform.position, true));
+    }
+
+    private IEnumerator Animate(Vector3 to, bool isMerging = false)
+    {
+        float elapsed = 0f;
+        float duration = 0.1f;
+
+        Vector3 from = transform.position;
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = to;
+
+        if (isMerging)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
