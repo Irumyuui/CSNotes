3ed0589d182414f387e3bc5dd12002a23b17e022
中宮 ちひろ <ciel_katana@outlook.com>
2024-09-09 22:13:20 +0800

    补档的一个简单的 ASP.NET api 项目

---
+++ WebDevelopment/Example/ASP.NET-Exp/BlazorAppTest.Entites/Dtos/CharacterDto.cs
@@ -0,0 +1,3 @@
+﻿namespace BlazorAppTest.Entites.Dtos;
+
+public record CharacterDto(Guid Id, string? NickName, string? Classes, int Level, DateTime DateCreated);
---
+++ WebDevelopment/Example/ASP.NET-Exp/BlazorAppTest.Entites/Dtos/PlayerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppTest.Entites.Dtos;
+
+public class PlayerDto
+{
+    public Guid Id { get; set; }
+    public string? Account { get; set; }
+    public string? AccountType { get; set; }
+    public DateTime DateCreated { get; set; }
+}
