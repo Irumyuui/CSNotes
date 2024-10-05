1ea4e3aacd6f627f197d26b5374edce0461f61c5
中宮 ちひろ <ciel_katana@outlook.com>
2024-10-05 21:10:03 +0800

    提交 ASP.NET DEMO

---
+++ WebDevelopment/BackEnd/AspNetWeb/JwtAuthDemo/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Permissions.UserCreate)]
+        public ActionResult<string> UserCreate() => "UserCreate";
+
+        [HttpGet]
+        [Authorize(Permissions.UserUpdate)]
+        public ActionResult<string> UserUpdate() => "UserUpdate";
+
+        [HttpGet]
+        [Authorize(Permissions.UserDelete)]
+        public ActionResult<string> UserDelete() => "UserDelete";
+    }
+}
